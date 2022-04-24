64a96799b77d5b5aff6591d47f36b8d9eee4a1a1
Moritz Ellerbrock <github@elmoritz.eu>
2022-04-24 23:50:06 +0200

    add more routes to config

--- Sources/CommonVapor/ModuleInterface.swift
+++ Sources/CommonVapor/ModuleInterface.swift
@@ -2,19 +2,21 @@ import Vapor
 import Fluent
 
 public protocol ModuleInterface {
-    var router: RouteCollection? { get }
+    var routers: [RouteCollection] { get }
     var migrations: [Migration] { get }
     
     func configure(_ app: Application) throws
 }
 
 public extension ModuleInterface {
-    
     func configure(_ app: Application) throws {
+        // add migrations
         for migration in migrations {
             app.migrations.add(migration)
         }
-        if let router = router {
+        
+        // register Routes
+        for router in routers {
             try router.boot(routes: app.routes)
         }
     }
